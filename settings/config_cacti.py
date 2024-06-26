98e8e0ecb6252078468504a00de28e7708968a08
Mrzhouxf <markino@163.com>
2024-06-26 16:52:09 +0800

    first commit

---
+++ settings/config_cacti.py
@@ -0,0 +1,4 @@
+cact_bin_addr = "D:/master_paper/code_sever/cacti_for_FARSI-main/cacti"
+cacti_param_addr = "D:/master_paper/code_sever/cacti_for_FARSI-main/farsi_gen.cfg"
+cacti_data_log_file = "D:/master_paper/code_sever/cacti_for_FARSI-main/data_log.csv"
+
